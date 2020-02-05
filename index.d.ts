f7fb5d5c085582276d55df6f49db0b52d09272f8
Luke Edwards <luke.edwards05@gmail.com>
2020-02-05 13:19:52 -0800

    fix(types): on & listen give unsubscribers

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
-export type Listener<T> = (state: T, prevState: T) => any;
 export type Handler<T, X> = (state: T, data: X) => T | void;
+export type Listener<T> = (state: T, prevState: T) => any;
+export type Unsubscriber = () => void;
 
 export interface Store<T, M> {
 	readonly state: T;
@@ -7,11 +8,11 @@ export interface Store<T, M> {
 	set(state: T): void;
 	set<K extends keyof M>(event: K | '*', state: T): void;
 
-	on<K extends keyof M>(event: K, handler: Handler<T, M[K]>): void;
+	on<K extends keyof M>(event: K, handler: Handler<T, M[K]>): Unsubscriber;
 	dispatch<K extends keyof M>(event: K, data: M[K]): Promise<void>;
 
-	listen(func: Listener<T>): void;
-	listen<K extends keyof M>(event: K | '*', func: Listener<T>): void;
+	listen(func: Listener<T>): Unsubscriber;
+	listen<K extends keyof M>(event: K | '*', func: Listener<T>): Unsubscriber;
 }
 
 export default function<M, T>(obj: T): Store<T, M>;
